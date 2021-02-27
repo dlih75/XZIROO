dc92956bc80fc360860a4748e246e400736a467a
hmjjj <74424096+dlih75@users.noreply.github.com>
2021-02-27 21:33:47 +0300

    Add files via upload

---
+++ ish.py
@@ -0,0 +1,40 @@
+import os 
+from time import sleep as XZIRO
+from os import system as ZIRO
+
+
+ziro = """
+
+Rekxstny barnamay ish
+
+Author:ZIRO
+Telegram@XZIRO12
+
+
+"""
+print (ziro)
+XZIRO(1)
+ZIRO('wget -qO- http://dl-cdn.alpinelinux.org/alpine/v3.12/main/x86/apk-tools-static-2.10.5-r1.apk | tar -xz sbin/apk.static && ./sbin/apk.static add apk-tools && rm sbin/apk.static && rmdir sbin 2> /dev/null ; python2 -m ensurepip ; pip install —upgrade pip ; pip2 install requests mechanize ; pip2 install python')
+print'\033[33m==\033[95m16%'
+XZRO(1)
+ZIRO('apk update ; apk upgrade ; apk add python ; apk add python2 ; apk add ruby ; apk add git ; apk add php ; apk add perl ; apk add nmap ; apk add bash ; apk add clang ; apk add macchanger')
+print'\033[33m===\033[96m36%'
+XZIRO(1)
+ZIRO('apk add nano ; apk add figlet ; apk add cowsay ; apk add curl ; apk add tar ; apk add py-pip')
+print'\033[33m====\033[92m47%'
+XZIRO(1)
+ZIRO('apk add zip ; apk add unzip ; apk add tor ; apk add sudo ; apk add wget')
+print'\033[33m=====\033[94m59%'
+XZIRO(1)
+ZIRO('apk add wcalc ; apk add openssl ; apk add bmon')
+print'\033[33m======\033[91m69%'
+XZIRO(1)
+ZIRO('apk add github ; apk add openssh')
+print'\033[33m=======\033[93m84%'
+
+ZIRO('apk add g++ ; apk add gcc')
+print'\033[33m========\033[95m96%'
+ZIRO('apk add python3 ; apk add py-pip')
+print'\033[33m=========\033[96m100%'
+ZIRO('cd .. ; mkdir HAMA ; cd HAMA ; touch AM ISHSHELL REKXRA LALAYAN ZIRO.py')
+ZIRO('cd ; cd /etc ; rm -rif motd ; cd ; cd rekxstn-ishshell ; mv motd $HOME ; cp motd /etc')
